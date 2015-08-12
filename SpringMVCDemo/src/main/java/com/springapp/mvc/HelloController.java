654efca0cfc5739f5eb1f810aee873829cb21309
daisongsong <daisongsong@rd.com>
2015-08-12 14:17:41 +0800

    添加显示服务器时间的page

--- SpringMVCDemo/src/main/java/com/springapp/mvc/HelloController.java
+++ SpringMVCDemo/src/main/java/com/springapp/mvc/HelloController.java
@@ -5,16 +5,25 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
     @RequestMapping(method = RequestMethod.GET)
     public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hello world!");
         model.addAttribute("msg", "Hello Spring!");
-
-        String name = (String) model.getOrDefault("name", "guest");
-        model.addAttribute("name", name);
         return "hello";
+    }
+
+    @RequestMapping("serverTime")
+    public String printServerTime(ModelMap modelMap) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        modelMap.addAttribute("time", dateFormat.format(new Date()));
+        return "server/serverTime";
     }
 }
